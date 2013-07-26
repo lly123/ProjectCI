aa5a10dc5c111633888e313e4571d85c252df67e
lyli <lly123@gmail.com>
2013-07-26 09:56:12 +0800

    2017-7-26

--- src/main/java/com/freeroom/projectci/beans/ReportType.java
+++ src/main/java/com/freeroom/projectci/beans/ReportType.java
@@ -3,7 +3,7 @@ package com.freeroom.projectci.beans;
 public enum ReportType {
     UserStory(273), FunctionalTesting(60), HighLevelDesign(10), Document(20),
     QSA(3), PerformanceTesting(20), IntegrationTesting(10),
-    OverTime(999), BugFixing(60), Leave(120);
+    OverTime(999), BugFixing(60), Leave(120), Others(999);
 
     private long estimatedEffort;
 
